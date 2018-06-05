79c648d0e7e6a480e7a1b0d45aa3e00c3402141c
Emily A Shaw <emily.shaw123@hotmail.com>
2018-06-05 16:11:49 +1200

    djhjdsd

--- public/app.js
+++ public/app.js
@@ -83,8 +83,10 @@ function googleLogin() {
 			var ThisUser = database.collection('Users').doc(String(UserID));
 			ThisUser.onSnapshot(doc =>{
 				const data = doc.data();
-				document.querySelector( "#Google").innerHTML = ("Welcome, " + data.name);
+				document.querySelector( "#Google" ).innerHTML = ("Welcome, " + data.name);
 			})
+
+
 		})
 			//.catch(console.log)
 }
@@ -200,6 +202,8 @@ function addFriendDocument() {
 	});
 }
 
+
+
 function hide(myDIV) {
 	myDIV = String(myDIV);
 	//console.log(myDIV);
@@ -209,13 +213,31 @@ function hide(myDIV) {
     } 
 
     else if (x.style.display ==="block" ){
-    	console.log("fella");
+    	
 
 }
     	else {
         x.style.display = "none";
     }
 }
+
+function show(myDIV) {
+	myDIV = String(myDIV);
+	//console.log(myDIV);
+    var x = document.getElementById(myDIV);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } 
+
+    else if (x.style.display ==="none" ){
+    	
+
+}
+    	else {
+        x.style.display = "block";
+    }
+}
+
 
 function HideStuffOnLogin(){
 	hide("EventForm");
@@ -227,6 +249,8 @@ function HideStuffOnLogin(){
 	hide("addEventLocation");
 	hide("GoogleButton");
 	hide("hideLogo");
+	
+
 
 	EventDropDown();
 	myMap();
@@ -238,7 +262,7 @@ function myMap() {
     var mapOptions = {
         center: Location,
         zoom: 15,
-      
+
         //mapTypeId: google.maps.MapTypeId.TERRAIN
 
         styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#6195a0"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":"0"},{"saturation":"0"},{"color":"#f5f5f2"},{"gamma":"1"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"lightness":"-3"},{"gamma":"1.00"}]},{"featureType":"landscape.natural.terrain","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#bae5ce"},{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#fac9a9"},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#787878"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station.airport","elementType":"labels.icon","stylers":[{"hue":"#0a00ff"},{"saturation":"-77"},{"gamma":"0.57"},{"lightness":"0"}]},{"featureType":"transit.station.rail","elementType":"labels.text.fill","stylers":[{"color":"#43321e"}]},{"featureType":"transit.station.rail","elementType":"labels.icon","stylers":[{"hue":"#ff6c00"},{"lightness":"4"},{"gamma":"0.75"},{"saturation":"-68"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#eaf6f8"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c7eced"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":"-49"},{"saturation":"-53"},{"gamma":"0.79"}]}]
