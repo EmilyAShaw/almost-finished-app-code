c01bc3ebfbc5a5182de17cd2846a1d223d18da58
Emily A Shaw <emily.shaw123@hotmail.com>
2018-06-05 16:19:47 +1200

    got geolocation working

--- public/app.js
+++ public/app.js
@@ -258,6 +258,7 @@ function HideStuffOnLogin(){
 
 function myMap() {
 	geocoder = new google.maps.Geocoder();
+	infoWindow = new google.maps.InfoWindow;
     var Location = new google.maps.LatLng(-41.291002,  174.768925)
     var mapOptions = {
         center: Location,
@@ -295,6 +296,34 @@ function myMap() {
     .catch(function(error) {
         console.log("Error getting documents: ", error);
     });
+
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      
 }
 
 function codeAddress() {
